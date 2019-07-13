07a437a285b4d503f2ab9fb84ed24c22bd814bc8
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-13 03:39:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    self.items << total * quantity
+    self.items << title * quantity
     self.total += (price * quantity)
   end
   
